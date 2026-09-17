3e9242033aa932a7bdd2436457bdb7860c8e348d
Lailouezzz <alanlebouder@gmail.com>
2026-09-17 08:23:53 +0000

    Convert if to switch statment

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ale-boud <ale-boud@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/17 10:01:09 by ale-boud          #+#    #+#             */
-/*   Updated: 2026/09/17 10:04:54 by ale-boud         ###   ########.fr       */
+/*   Updated: 2026/09/17 10:23:39 by ale-boud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@
 #include <getopt.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdnoreturn.h>
 #include <sysexits.h>
 
 #include "ping.h"
@@ -47,6 +48,7 @@ t_ping_ctx	g_ctx = {
  */
 static struct option	long_opts[] = {
 	{"help", no_argument, NULL, 'h'},
+	{"verbose", no_argument, NULL, 'v'},
 	{NULL, 0, NULL, 0},
 };
 
@@ -59,14 +61,13 @@ void	print_usage(void)
 	printf("Usage: ft_ping [OPTION...] HOST ...\n");
 }
 
-void	print_help(void)
+noreturn void	print_help(void)
 {
 	print_usage();
 	printf("Send ICMP ECHO_REQUEST packets to network hosts.\n");
 	printf("\n");
 	printf("  -v, --verbose              verbose output\n");
 	printf("  -?, --help                 give this help list\n");
-	printf("\n");
 	exit(EXIT_SUCCESS);
 }
 
@@ -83,35 +84,30 @@ int	main(
 	return (EXIT_SUCCESS);
 }
 
-/**
- * @brief Parse command-line arguments with getopt_long and fill g_ctx.
- * @param argc Argument count from main.
- * @param argv Argument vector from main.
- */
 void	parse_args(int argc, char **argv)
 {
 	int			c;
 	const char	*tok;
 
 	opterr = 0;
-	while ((c = getopt_long(argc, argv, "h?", long_opts, NULL)) != -1)
+	while ((c = getopt_long(argc, argv, "hv?", long_opts, NULL)) != -1)
 	{
-		if (c == 'h')
-			print_help();
-		else if (c == '?')
-		{
-			tok = argv[optind - 1];
-			/* -? is registered like any other short opt: getopt_long also
-			 * returns '?' for an unknown option, so tell them apart via
-			 * optopt (unset for -?) and the "--" prefix (long opt errors). */
-			if (optopt == 0 && !(tok[0] == '-' && tok[1] == '-'))
+		switch (c) {
+			case 'h':
 				print_help();
-			if (tok[0] == '-' && tok[1] == '-')
-				error_msg("unrecognized option '%s'", tok);
-			else
-				error_msg("invalid option -- '%c'", optopt);
-			fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
-			exit(EX_USAGE);
+			case 'v':
+				set_verbose(true);
+				break ;
+			case '?':
+				tok = argv[optind - 1];
+				if (optopt == 0 && !(tok[0] == '-' && tok[1] == '-'))
+					print_help();
+				if (tok[0] == '-' && tok[1] == '-')
+					error_msg("unrecognized option '%s'", tok);
+				else
+					error_msg("invalid option -- '%c'", optopt);
+				fprintf(stderr, "Try '%s --help' for more information.\n", argv[0]);
+				exit(EX_USAGE);
 		}
 	}
 	if (optind >= argc)
